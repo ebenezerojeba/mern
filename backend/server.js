6a9d34cb26416671eca1862a93be5181c1e67f32
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2024-07-31 09:40:59 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express()
 const port = process.env.PORT || 4000;
 
 //CORS
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+const allowedOrigins = ['https://palatial.onrender.com', 'http://localhost:5174'];
 
 app.use(cors({
     origin: function (origin, callback) {
