e9b3e9cac16ecabf5ccf23d29c9dc442052f01f8
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2024-08-01 12:58:19 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,27 +32,6 @@ const allowedOrigins = [
     optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
   }));
   
-{/*
-const allowedOrigins = ['https://palatial.onrender.com', 'https://palatial-admin.onrender.com', 'http://localhost:5173', 'http://localhost:5ll174'];
-=======
-const allowedOrigins = ['https://palatial.onrender.com', 'https://palatial-admin.onrender.com', 'http://localhost:5173','http://localhost:5174'];
->>>>>>> 519e88f0766e8bb5dcfd035f22fcbc0cd4118735
-
-app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin, like mobile apps or curl requests
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-})); */}
-
-
-
 // middleware
 app.use(express.json());
 
