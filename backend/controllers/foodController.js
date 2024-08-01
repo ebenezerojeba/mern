f72d494dfcebe57cb5edf7141bcbf85a897a5fe7
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2024-08-01 14:31:26 +0100

    Update foodController.js

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -44,7 +44,7 @@ const removeFood = async (req,res) => {
         fs.unlink(`uploads/${food.image}`,()=>{})
 
         await foodModel.findByIdAndDelete(req.body.id);
-        res.json({succes:true,message:"Food Removed"})
+        res.json({success:true,message:"Food Removed"})
     } catch (error) {
         console.log(error);
         res.json({success:false,message:"Error"})
