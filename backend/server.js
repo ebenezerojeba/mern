d0f269ace684d0b7e4d1be5212bfacf8e63f37ae
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2024-08-01 14:03:57 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,55 +1,55 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRouter.js"
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRouter.js";
+import 'dotenv/config';
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
 
-//CORS
+// CORS
 const allowedOrigins = [
     'https://palatial.onrender.com',
     'https://palatial-admin.onrender.com',
     'http://localhost:5173',
     'http://localhost:5174'
-  ];
-  
-  app.use(cors({
+];
+
+app.use(cors({
     origin: function (origin, callback) {
-      // Allow requests with no origin, like mobile apps or curl requests
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
+        // Allow requests with no origin, like mobile apps or curl requests
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
     },
     optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  }));
-  
+}));
+
 // middleware
 app.use(express.json());
 
-//db connection
+// db connection
 connectDB();
 
-//api endpoints
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
+app.get("/", (req, res) => {
+    res.send("API Working");
+});
 
-
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
-
-app.listen(
-    console.log(`Server Stared on https://palatial-backend.onrender.com `)
-)
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
