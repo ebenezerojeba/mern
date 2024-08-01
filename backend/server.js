9b1cfc6b597fd9c3bdca9a315458f032f0062912
ebenezerojeba <69347304+ebenezerojeba@users.noreply.github.com>
2024-08-01 13:18:59 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,7 @@ import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
 
 // app config
-const app = express()
-const port = process.env.PORT || 5000;
+const app = express();
 
 //CORS
 const allowedOrigins = [
